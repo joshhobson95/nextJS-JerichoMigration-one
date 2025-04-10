d35365ac1005b89a2c71de3f1acee3a37a090c80
joshhobson95 <joshuahobson1995@gmail.com>
2025-04-10 12:49:36 -0600

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import styles from './Header.module.css'; // Assuming you want to use CSS modules for styling
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/aboutus">About Us</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/sales">Sales</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/plantcatalog">Plant Catalog</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,14 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import { Raleway } from "next/font/google";
-
+import Header from "@/components/Header/Header";
 
 const raleway = Raleway({subsets: ['latin']})
 
 export default function App({ Component, pageProps }) {
   return (
     <div className={raleway.className}>
+      <Header />
       <Component {...pageProps} />
     </div>
   )
