0967cda501f99fb0249925d0b34b3aacee2ca800
joshhobson95 <joshuahobson1995@gmail.com>
2025-04-25 13:13:49 -0600

    robots.txt modification

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,53 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.jerichonursery.com/',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://www.jerichonursery.com/',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*', // Apply to all bots
+        disallow: ['/'], // Block all access to the site for non-permitted bots
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/', // Allow Googlebot to crawl the entire site
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/', // Allow Bingbot
+      },
+      {
+        userAgent: 'Slurp',
+        allow: '/', // Allow Yahoo's bot
+      },
+      {
+        userAgent: 'DuckDuckBot',
+        allow: '/', // Allow DuckDuckGo bot
+      },
+      {
+        userAgent: 'AhrefsBot', // Block AhrefsBot (can be used for scraping)
+        disallow: '/',
+      },
+      {
+        userAgent: 'SemrushBot', // Block SemrushBot (can be used for scraping)
+        disallow: '/',
+      },
+      {
+        userAgent: 'MJ12bot', // Block MJ12bot (another bot often used for scraping)
+        disallow: '/',
+      },
+      {
+        userAgent: 'baidu', // Block Baidu's bot
+        disallow: '/',
+      },
+      {
+        userAgent: 'YandexBot', // Block YandexBot (bot from Russia, often used for scraping)
+        disallow: '/',
+      },
+      // You can add more malicious bots here by adding their user-agent and disallowing them
+    ],
+    additionalSitemaps: [
+      'https://www.jerichonursery.com/sitemap.xml', // Ensure your sitemap is included
+    ],
+  },
+};
