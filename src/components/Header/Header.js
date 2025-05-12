5bc3b2b458284be658debcdb3818ff7c6d5a66dc
joshhobson95 <joshuahobson1995@gmail.com>
2025-05-12 15:37:29 -0600

    rework header to fix cls

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,36 @@
 'use client';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import NavBarDesktop from './NavBarDesktop';
 import NavBarMobile from './NavBarMobile';
 import RotatingBanner from '../RotatingBanner/RotatingBanner';
+import Image from 'next/image';
 import logo from '../../../public/assets/Logo.png';
-import styles from './HeaderStyles.module.css'
-
+import styles from './HeaderStyles.module.css';
 
 function Header() {
-  const [isMobile, setIsMobile] = useState(null); 
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleResize = () => {
-        setIsMobile(window.innerWidth < 500);
-      };
-
-      handleResize();
-      window.addEventListener('resize', handleResize);
-
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }
-  }, []);
-
-
-  if (isMobile === null) {
-    return <div>Loading...</div>; 
-  }
-
   return (
     <div className={styles.header_wrapper}>
-      {isMobile ? (
-        <div>
-          <NavBarMobile />
-          <div className={styles.mobile_photo_banner}>
-            <RotatingBanner />
-            <Link href='/'>
-              <div className={styles.logo_div}>
+      {/* Mobile Header */}
+      <div className={styles.mobile_header}>
+        <NavBarMobile />
+        <div className={styles.mobile_photo_banner}>
+          <RotatingBanner />
+          <Link href='/'>
+            <div className={styles.logo_div}>
                 <img
                   className={styles.mobile_banner_logo} 
                   src={logo.src} 
                   alt='logo' 
                 />
-              </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
         </div>
-      ) : (
+      </div>
+
+      {/* Desktop Header */}
+      <div className={styles.desktop_header}>
         <NavBarDesktop />
-      )}
+      </div>
     </div>
   );
 }
