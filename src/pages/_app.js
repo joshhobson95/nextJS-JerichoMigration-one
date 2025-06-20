ff82843906dd7e1830823cb96d47cc0974f7b7dd
joshhobson95 <joshuahobson1995@gmail.com>
2025-06-20 15:58:06 -0600

    Google Analytics Tag added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,15 +5,48 @@ import "swiper/css/navigation";
 import { Raleway } from "next/font/google";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-const raleway = Raleway({subsets: ['latin']})
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Optional: track route changes as pageviews
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag?.("config", "G-QTXV1CLD6E", {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
   return (
     <div className={raleway.className}>
+      {/* Google Analytics Scripts */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-QTXV1CLD6E"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QTXV1CLD6E');
+        `}
+      </Script>
+
       <Header />
       <Component {...pageProps} />
       <Footer />
     </div>
-  )
+  );
 }
