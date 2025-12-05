f3ec0793e47b1a6c0a503b0a1024ed73012c7945
Nick Pecastaing <Nick@EHP.LOCAL>
2025-12-05 09:42:03 -0700

    Add Google Tag Manager (GTM-T67BZKK) via _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,18 +3,50 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-<Head>
-  <link rel="icon" href="/favicon.png" type="image/png" />
-  <link rel="apple-touch-icon" href="/favicon.png" />
-  <meta name="theme-color" content="#242424" />
-  <meta property="og:image" content="/favicon.png" />
-  <meta name="twitter:image" content="/favicon.png" />
-  <link rel="manifest" href="/manifest.json" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=yard" />
-  <link
-  rel="preload" href="/_next/static/media/slick.653a4cbb.woff" as="font" type="font/woff" crossorigin="anonymous"/>
-</Head>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta name="theme-color" content="#242424" />
+        <meta property="og:image" content="/favicon.png" />
+        <meta name="twitter:image" content="/favicon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=yard"
+        />
+        <link
+          rel="preload"
+          href="/_next/static/media/slick.653a4cbb.woff"
+          as="font"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T67BZKK');
+            `,
+          }}
+        />
+      </Head>
+
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T67BZKK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
